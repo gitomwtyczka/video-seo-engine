85ef818b49434993b0913322a7feb42fcbb95f57
gitomwtyczka <tomasz.brzozowski@impresjapr.pl>
2026-06-16 23:53:18 +0200

    feat: export WpPortal from models __init__ [vse-dev-12]

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,3 +1,4 @@
 from api.models.user import User, Plan, UsageLog, ApiKey, PlanName
+from api.models.portal import WpPortal
 
-__all__ = ["User", "Plan", "UsageLog", "ApiKey", "PlanName"]
+__all__ = ["User", "Plan", "UsageLog", "ApiKey", "PlanName", "WpPortal"]
