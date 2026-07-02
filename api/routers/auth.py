9af4c0a104ff9d37e333b8b8b90ba791cae97d25
gitomwtyczka <tomasz.brzozowski@impresjapr.pl>
2026-07-02 16:43:44 +0200

    fix: usuniecie /api z linku google auth callback w auth.py [[vse-dev-01]]

--- api/routers/auth.py
+++ api/routers/auth.py
@@ -28,7 +28,7 @@ GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID", "")
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET", "")
 GOOGLE_REDIRECT_URI = os.getenv(
     "GOOGLE_REDIRECT_URI",
-    "https://vse.impresjapr.pl/api/v1/auth/google/callback"
+    "https://vse.impresjapr.pl/v1/auth/google/callback"
 )
 
 
