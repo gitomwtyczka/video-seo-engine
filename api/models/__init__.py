430dec974b4a7e76c3f966110dffd4db573fb413
gitomwtyczka <tomasz.brzozowski@impresjapr.pl>
2026-07-10 20:36:46 +0200

    add youtubechannel to models init [vse-dev-01]

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,4 +1,5 @@
 from api.models.user import User, Plan, UsageLog, ApiKey, PlanName
 from api.models.portal import WpPortal
+from api.models.youtube_channel import YouTubeChannel
 
-__all__ = ["User", "Plan", "UsageLog", "ApiKey", "PlanName", "WpPortal"]
+__all__ = ["User", "Plan", "UsageLog", "ApiKey", "PlanName", "WpPortal", "YouTubeChannel"]
