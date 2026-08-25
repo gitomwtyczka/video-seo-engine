09953bc35882b141e15162c9a1ff55014852e905
gitomwtyczka <tomasz.brzozowski@impresjapr.pl>
2026-08-25 23:47:35 +0200

    fix: add ShortCandidateSet and ShortJob to models __init__ [sonic-void]

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -2,5 +2,7 @@ from api.models.user import User, Plan, UsageLog, ApiKey, PlanName
 from api.models.portal import WpPortal
 from api.models.youtube_channel import YouTubeChannel
 from api.models.oauth_state import OAuthState
+from api.models.short_candidate import ShortCandidateSet
+from api.models.short_job import ShortJob
 
-__all__ = ["User", "Plan", "UsageLog", "ApiKey", "PlanName", "WpPortal", "YouTubeChannel", "OAuthState"]
+__all__ = ["User", "Plan", "UsageLog", "ApiKey", "PlanName", "WpPortal", "YouTubeChannel", "OAuthState", "ShortCandidateSet", "ShortJob"]
